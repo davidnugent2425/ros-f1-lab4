d70b525e3180c081fc2ce87b563e308c5e24e2dc
David Nugent <david.nugent2425@gmail.com>
2021-03-25 11:43:05 +0000

    driver script now working pretty well

---
+++ driver.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import math
+import numpy as np
+
+#ROS Imports
+import rospy
+from sensor_msgs.msg import Image, LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+
+
+class reactive_follow_gap:    
+
+    BUBBLE_RADIUS = 160
+    PREPROCESS_CONV_SIZE = 3
+    BEST_POINT_CONV_SIZE = 80
+    MAX_LIDAR_DIST = 3000000
+    STRAIGHTS_SPEED = 4.0
+    CORNERS_SPEED = 0.5
+    STRAIGHTS_STEERING_ANGLE = math.pi / 9  # 20 degrees
+    
+    def __init__(self):
+        #Topics & Subscriptions,Publishers
+        lidarscan_topic = '/scan'
+        drive_topic = '/nav'
+
+        self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan,
+                self.lidar_callback, queue_size=1, buff_size=2**24)
+        self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped,
+                queue_size=1)
+        
+        # used when calculating the angles of the LiDAR data
+        self.degrees_per_elem = None
+    
+    def preprocess_lidar(self, ranges):
+        """ Preprocess the LiDAR scan array. Expert implementation includes:
+            1.Setting each value to the mean over some window
+            2.Rejecting high values (eg. > 3m)
+        """
+        self.degrees_per_elem = (2*math.pi) / len(ranges)
+        proc_ranges = np.array(ranges[135:-135])
+        # sets each value to the mean over a given window
+        proc_ranges = np.convolve(proc_ranges, np.ones(self.PREPROCESS_CONV_SIZE), 'same') / self.PREPROCESS_CONV_SIZE
+        proc_ranges = np.clip(proc_ranges, 0, self.MAX_LIDAR_DIST)
+        return proc_ranges
+
+    def find_max_gap(self, free_space_ranges):
+        """ Return the start index & end index of the max gap in free_space_ranges
+            free_space_ranges: list of LiDAR data which contains a 'bubble' of zeros
+        """
+        # mask the bubble
+        masked = np.ma.masked_where(free_space_ranges==0, free_space_ranges)
+        # get a slice for each contigous sequence of non-bubble data
+        slices = np.ma.notmasked_contiguous(masked)
+        max_len = slices[0].stop - slices[0].start
+        chosen_slice = slices[0]
+        # I think we will only ever have a maximum of 2 slices but will handle an
+        # indefinitely sized list for portablility
+        for sl in slices[1:]:
+            sl_len = sl.stop - sl.start
+            if sl_len > max_len:
+                max_len = sl_len
+                chosen_slice = sl
+        return chosen_slice.start, chosen_slice.stop
+    
+    def find_best_point(self, start_i, end_i, ranges):
+        """Start_i & end_i are start and end indices of max-gap range, respectively
+        Return index of best point in ranges
+	Naive: Choose the furthest point within ranges and go there
+        """
+        averaged_max_gap = np.convolve(ranges[start_i:end_i], np.ones(self.BEST_POINT_CONV_SIZE), 'same') / self.BEST_POINT_CONV_SIZE
+        return averaged_max_gap.argmax() + start_i
+
+    def get_angle(self, range_index, range_len):
+        """ Get the angle of a particular element in the LiDAR data
+        """
+        return (range_index - (range_len/2)) * self.degrees_per_elem
+
+    def lidar_callback(self, data):
+        """ Process each LiDAR scan as per the Follow Gap algorithm & publish an AckermannDriveStamped Message
+        """
+        ranges = data.ranges
+        proc_ranges = self.preprocess_lidar(ranges)
+
+        #Find closest point to LiDAR
+        closest = proc_ranges.argmin()
+
+        #Eliminate all points inside 'bubble' (set them to zero)
+        min_index = closest - self.BUBBLE_RADIUS
+        max_index = closest + self.BUBBLE_RADIUS
+        if min_index < 0: min_index = 0
+        if max_index >= len(proc_ranges): max_index = len(proc_ranges)-1
+        proc_ranges[min_index:max_index] = 0
+
+        #Find max length gap
+        gap_start, gap_end = self.find_max_gap(proc_ranges)
+
+        #Find the best point in the gap 
+        best = self.find_best_point(gap_start, gap_end, proc_ranges)
+
+        #Publish Drive message
+        steering_angle = self.get_angle(best, len(proc_ranges))
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = rospy.Time.now()
+        drive_msg.drive.steering_angle = steering_angle
+        if abs(steering_angle) > self.STRAIGHTS_STEERING_ANGLE:
+            drive_msg.drive.speed = self.CORNERS_SPEED
+        else: drive_msg.drive.speed = self.STRAIGHTS_SPEED
+        self.drive_pub.publish(drive_msg)
+        rospy.loginfo('Steering angle in degrees: %f', steering_angle*180)
+
+def main(args):
+    rospy.init_node("FollowGap_node", anonymous=True)
+    rfgs = reactive_follow_gap()
+    rospy.sleep(0.1)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main(sys.argv)
